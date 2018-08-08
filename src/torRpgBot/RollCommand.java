534a04f5d07b266603ed976b967045a53481858c
Seosaidh <42014971+Seosaidh@users.noreply.github.com>
2018-08-08 14:31:01 -0400

    Added Full Comments to RollCommand.java
    Added full comments, including javadoc support, to the RollCommand.java file.

--- src/torRpgBot/RollCommand.java
+++ src/torRpgBot/RollCommand.java
@@ -34,6 +34,14 @@ public class RollCommand extends TorDice{
 		super(flag);
 	}
 
+	
+	/**
+	* This is an override of the {@link torRpgBot.Command#getAliases Command class getAliases} function. 
+	* It simply returns a hard-coded list of command aliases for the Roll command. The possible aliases
+	* are: "r" and"roll". If the first word (less the commandFlag string) matches one of these
+	* strings, then the RollCommand class will handle the event.
+	* @return List of String objects containing the roll aliases.
+	*/
 	@Override
 	protected List<String> getAliases() {
 		List<String> aliases = new ArrayList<String>();
@@ -42,6 +50,16 @@ public class RollCommand extends TorDice{
 		return aliases;
 	}
 
+	
+	/**
+	* This is an override of the {@link torRpgBot.Command#handleCommand Command class handleCommand} function. 
+	* This function will first pull out the Guild, the User author, and the String message string from the event.
+	* Then, it performs a quick initial check on the command string before passing it (along with the Guild and author)
+	* to the {@link torRpgBot.TorDice#handleRollCommand handleRollCommand} function with the isAdversary parameter
+	* set to false. Once that function returns, this function will check to see if there is a string response to send back
+	* on the Discord channel. If so, it will send this response.
+	* @param event A MessageReceivedEvent class that contains all the information from the Discord event that will be handled by this function
+	*/
 	@Override
 	protected void handleCommand(MessageReceivedEvent event) {
 		String result;
@@ -72,6 +90,11 @@ public class RollCommand extends TorDice{
     	}
 	}
 
+	/**
+	* This is an override of the {@link torRpgBot.Command#getHelp Command class getHelp} function. 
+	* This function simply returns a hard-coded string that describes the proper way to call the Adversary command.
+	* @return String containing the help text.
+	*/
 	@Override
 	protected String getHelp() {
 		return "**Roll Command**\n"
